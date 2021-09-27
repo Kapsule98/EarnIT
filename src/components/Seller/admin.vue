42906f1420f347c6aaa94e926996b1212bf6c234
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-09-27 22:01:28 +0530

    admin ui improved

--- src/components/Seller/admin.vue
+++ src/components/Seller/admin.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="admin_all">
+  <b-card class="admin_all">
     <h1 style="margin: 30px 20px">
       Your request has been recived by us. Try to login after some time!
     </h1>
     <h1 style="margin: 30px 20px; color: white">
-      <router-link to="/"> Home</router-link>
+      <router-link to="/" style="float: right"> Home</router-link>
     </h1>
-  </div>
+  </b-card>
 </template>
 <script>
 export default {};
@@ -14,17 +14,7 @@ export default {};
 
 <style scoped>
 .admin_all {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgb(120, 200, 255);
-  background: radial-gradient(
-    circle,
-    rgba(120, 200, 255, 1) 20%,
-    rgba(20, 153, 113, 1) 100%,
-    rgba(230, 0, 255, 1) 100%
-  );
+  margin: 10px;
+  background: antiquewhite;
 }
 </style>
