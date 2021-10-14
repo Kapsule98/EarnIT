00b8f25af7b3596a81d4e93c0e537199c8360698
Kapsule98 <kapil9787@gmail.com>
2021-10-15 01:44:01 +0530

    rebuild

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'http://127.0.0.1:5000/api'
  //export const BASE_URL = "https://earnitapi.herokuapp.com/"
 // export const BASE_URL = process.env.VUE_APP_BASEURL
-export const BASE_URL = window.location.origin + '/api'
+export const BASE_URL = 'http://www.lemmebuy.in/api'
